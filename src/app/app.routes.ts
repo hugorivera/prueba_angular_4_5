6341ea07bc0e331a9a3c1ef45530a16eb1e8b311
hugorivera <riveraquevedoh@gmail.com>
2020-07-23 12:07:03 -0500

    Termino de seccion

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { USUARIO_ROUTES } from "./components/usuario/usuario.routes";
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { 
+        path: 'usuario/:id', 
+        component: UsuarioComponent,
+        children: USUARIO_ROUTES
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
+
+export const APP_ROUTING = RouterModule.forRoot(routes);
